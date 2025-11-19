554b8ece43a2103797da2faef3302d1cfc00837e
0xJasonChien <jason891588@gmail.com>
2025-11-19 11:44:53 +0800

    [feat] Add CI workflow, update models, serializers, and views for order processing
    - Create a CI workflow in  for Python linting and testing.
    - Update  to include .
    - Add pre-commit configuration in  for code quality checks.
    - Implement  in  and create  model.
    - Add  in  for order data validation.
    - Update  view in  to handle order creation with token validation.
    - Clean up unused imports and formatting in various files.

--- api/models.py
+++ api/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 
-# Create your models here.
-class Order(models.Model):
-    # Add your model here
-    pass
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=models.UUIDField, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='創建時間')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='最後更新時間')
 
+    class Meta:
+        abstract = True
+
+
+class Order(BaseModel):
+    order_number = models.AutoField(unique=True, verbose_name='訂單編號')
+    total_price = models.PositiveIntegerField(verbose_name='訂單總價')
--- api/views.py
+++ api/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponseBadRequest
+from rest_framework import status
 from rest_framework.decorators import api_view
-# Create your views here.
+from rest_framework.response import Response
 
+from .models import Order
+from .serializers import ImportOrderSerializer
 
-ACCEPTED_TOKEN = ('omni_pretest_token')
+ACCEPTED_TOKEN = ('omni_pretest_token',)
 
 
 @api_view(['POST'])
 def import_order(request):
-    # Add your code here
-    return HttpResponseBadRequest()
+    serializer = ImportOrderSerializer(data=request.data)
+
+    if serializer.is_valid():
+        return Response(serializer.validated_data, status=201)
+
+    data = serializer.validated_data
+
+    if data['token'] not in ACCEPTED_TOKEN:
+        return Response(
+            {'detail': 'Invalid token'},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
+
+    order = Order.objects.create(
+        amount=data['amount'],
+    )
+
+    return Response(
+        ImportOrderSerializer(data=order).data,
+    )
