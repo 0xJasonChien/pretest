f9f086bdfd6a9ba574c45093a10208df2526965c
Jason <jason891588@gmail.com>
2025-11-20 11:47:16 +0800

    [feat] enhance CI workflow, add test configurations, and update dependencies

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from api.views import import_order, import_product, list_product
 
 urlpatterns = [
-    path('import-order/', import_order),
-    path('import-product/', import_product),
-    path('list-product/', list_product),
+    path('import-order/', import_order, name='import_order'),
+    path('import-product/', import_product, name='import_product'),
+    path('list-product/', list_product, name='list_product'),
 ]
