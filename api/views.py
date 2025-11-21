08972d5684107aef9f6b8d3cb60c3f993a875733
0xJasonChien <jason891588@gmail.com>
2025-11-21 12:19:23 +0800

    [refactor] streamline order creation and response handling in import_order view

--- api/views.py
+++ api/views.py
@@ -13,40 +13,30 @@ from .serializers import CreateOrderSerializer, OrderListSerializer, ProductSeri
 @token_required
 def import_order(request: HttpRequest) -> Response:
     serializer = CreateOrderSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
-    if serializer.is_valid():
-        created_order, product_snapshot_list = Order.create_order(serializer)
+    created_order, created_product_list = Order.create_order(serializer)
 
-        response_data = {
-            'order_number': created_order.order_number,
-            'total_price': created_order.total_price,
-            'product': product_snapshot_list,
-        }
-        created_data = OrderListSerializer(response_data)
+    response_data = {
+        'order_number': created_order.order_number,
+        'total_price': created_order.total_price,
+        'product': created_product_list,
+    }
+    created_data = OrderListSerializer(response_data)
 
-        return Response(created_data.data, status=status.HTTP_201_CREATED)
-
-    return Response(
-        {'detail': serializer.errors},
-        status=status.HTTP_400_BAD_REQUEST,
-    )
+    return Response(created_data.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
 @token_required
 def import_product(request: HttpRequest) -> Response:
     serializer = ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
 
-    if serializer.is_valid():
-        product = serializer.save()
-        response_data = ProductSerializer(product)
+    product = serializer.save()
+    response_data = ProductSerializer(product)
 
-        return Response(response_data.data, status=status.HTTP_201_CREATED)
-
-    return Response(
-        {'detail': serializer.errors},
-        status=status.HTTP_400_BAD_REQUEST,
-    )
+    return Response(response_data.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET'])
